edddd84b51b7194cfbbb8ce1962aa438252ae94e
RicardoAlPe <70081035+RicardoAlPe@users.noreply.github.com>
2021-02-13 22:10:15 -0600

    Add files via upload

---
+++ Ricardo01/Negocio/CLSLogin.cs
@@ -0,0 +1,14 @@
+﻿using Ricardo01.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ricardo01.Negocio
+{
+    class CLSLogin
+    {
+        Login log = new Login();
+    }
+}
